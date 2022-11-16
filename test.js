e5ab8d3b8984f6740625639118c8d23e892d903b
Hrishikesh Shirsat <114989129+hrishi54@users.noreply.github.com>
2022-11-16 12:19:19 +0530

    Create test.js

---
+++ test.js
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>This is to test GithubConnector</h2>
+<p>pss.</p>
+
+<p id="demo"></p>
+
+<script>
+document.getElementById("demo").innerHTML = 5 + 6;
+</script>
+
+</body>
+</html> 
